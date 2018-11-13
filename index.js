cc9f1aca8de933cc7866eb91dd54f0aaf4407e24
John Cheng <cheng.john.r@gmail.com>
2018-11-13 17:24:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // add solution here
+
+
+function theBeatlesPlay(m, i) {
+  
+}
+
+function johnLennonFacts(facts) {
+  
+}
+
+function iLoveTheBeatles(n) {
+  arr = []
+  do {
+    arr.push("I love the Beatles!")
+  while (n < 15)
+  return arr
+}
